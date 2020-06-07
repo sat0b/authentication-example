2bd983c07ea446e0a579061f2556af495c326eb3
Junichi Sato <sat0b.3ee@gmail.com>
2020-06-07 19:24:01 +0900

    Use gatsby plugin

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,13 +2,6 @@ import React, { FC, useEffect } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.API_DOMAIN,
-};
-console.log(config);
-firebase.initializeApp(config);
-
 const uiConfig = {
   signInFlow: 'popup',
   signInSuccessUrl: '/signedIn',
