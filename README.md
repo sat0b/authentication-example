5a886c6cf46e72448237d4519eeaf45ce8b6f548
Junichi Sato <sat0b.3ee@gmail.com>
2020-06-15 04:39:45 +0900

    Update

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# farebase Example
+# farebase example
 
 Create Gatsby project
 
     $ gatsby new farebase-example https://github.com/haysclark/gatsby-starter-typescript
 
-Add react-firebaseui
+Add firebase plugins
 
-    $ yarn add firebase react-firebaseui
+    $ yarn add firebase gatsby-plugin-firebase react-firebaseui
+
+    $ yarn add firebase-tools
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,29 @@ require(`dotenv`).config({
 });
 module.exports = {
   siteMetadata: {
-    title: `farebase Example`,
+    title: `farebase example`,
   },
   plugins: [
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    {      
+      resolve: "gatsby-plugin-firebase",  
+       options: {      
+        features: {     
+         auth: true,       
+         database: false,  
+         firestore: true,
+         storage: false,    
+         messaging: true, 
+         functions: false,  
+         performance: false, 
+         analytics:true,    
+        }, 
+        credentials: {  
+         apiKey:'FIREBASE_API_KEY', 
+         authDomain:'API_DOMAIN',
+         projectId: 'FIREBASE_PROJECT_ID',
+       }   
+      },
+     },    
   ],
 }
